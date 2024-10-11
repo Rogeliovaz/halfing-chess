2a1a724c0f73b6ed25efeffda6b9583862480979
Nealke23 <143419309+Nealke23@users.noreply.github.com>
2024-10-11 14:58:05 -0400

    Add files via upload

---
+++ creatures.py
@@ -0,0 +1,19 @@
+x = 1
+class Villain:
+    pass
+class Hero:
+    pass
+class Skeleton:
+    pass
+class Goblin:
+    pass
+class Necromancer:
+    pass
+class Warrior:
+    pass
+class Mage:
+    pass
+class Paladin:
+    pass
+class Ranger:
+    pass
